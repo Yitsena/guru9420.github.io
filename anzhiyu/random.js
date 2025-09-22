6a2a2c1c07dd6d142180087aa10cabe6a9107a14
Yitsena <linuxcodeguru@gmail.com>
2025-09-22 16:17:59 +0800

    Site updated: 2025-09-22 16:17:58

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/09/21/Hexo搭建个人博客/","2025/09/21/hello-world/","2025/09/21/gi提交到远程/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
