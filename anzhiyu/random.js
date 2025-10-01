b9b51278fd8f8a44b5ada89a3ebf1f8a8fe992f8
Yitsena <linuxcodeguru@gmail.com>
2025-10-01 18:14:59 +0800

    Site updated: 2025-10-01 18:14:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/21/hello-world/","2025/09/21/Hexo搭建个人博客/","2025/09/21/gi提交到远程/"];function toRandomPost(){
+var posts=["2025/09/21/Hexo搭建个人博客/","2025/09/21/hello-world/","2025/09/21/gi提交到远程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
